ea4b1a5ddfb7bffdc5c7f8659135e43062222dd8
shri1437 <shrikantkajale1437@gmail.com>
2022-02-23 18:48:47 +0530

    4th problem

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,12 +5,21 @@ router.get('/test-me', function(req,res){
     res.send('my first ever api!')
 })
 
-router.get('/shows', function(req, res) {
- res.send([{id:1, name: "The Shining"}, {id:2, name: "Incendies"}, {id:3, name:"Rang de Basanti"}, {id:4, name:"Finding Demo"}])   
-        
-})
-
-
+router.get('/films/:filmId', function (req,res) {
+    let movi = [ {id: 1,name: 'The Shining'}, {id: 2, name: 'Incendies'},  {id: 3, name: 'Rang de Basanti'}, {id: 4, name: 'Finding Demo'} ]
+    let value = req.params.filmId;
+    let found = false;
+    for(i=0; i<movi.length;i++) {
+        if(movi[i].id==value) {
+            found = true
+            res.send(movi[i])
+            break
+        }
+    }
+    if(found==false) {
+        res.send('No Movie exists with this id') 
+    }
+});
 
 
 module.exports = router;
