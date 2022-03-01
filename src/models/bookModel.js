2ab0179bb655e1a83eff43b5f888f8bba0eb6219
shri1437 <shrikantkajale1437@gmail.com>
2022-03-01 16:19:06 +0530

    Book Model with 6 API

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,44 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
+    bookName: {
+        type: String,
+        unique: true,
+        required: true
+    }, 
+
     authorName: String, 
+
     tags: [String],
+
+    totalPages: Number, 
     
-    isPublished: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+
+    year: {type: Number, default: 2021},
+
+    stockAvailable: Boolean
+
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+//users
 
 //Validation:
 //require:true
